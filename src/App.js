fd4fc3d35ab960f1a1eb2e4edce1ffd012100f94
Abrarul-Haque1303 <abrararko8691@gmail.com>
2026-02-06 22:52:07 +0600

    Add System Management Feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
 import PublicRoute from "./routes/PublicRoute";
-import PrivateRroute from "./routes/PrivateRoute";
+import PrivateRoute from "./routes/PrivateRoute";
 
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
+import SystemManagement from "./pages/SystemManagement";
 
 function App() {
   return (
@@ -16,6 +18,9 @@ function App() {
           <Route path="/register" element={<PublicRoute> <Register /> </PublicRoute>} />
           <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
           <Route path="/forgot-password" element={<PublicRoute> <ForgotPassword /></PublicRoute>} />
+
+          
+          <Route path="/system-management" element={<PrivateRoute> <SystemManagement /> </PrivateRoute>} />
          
         </Routes>
       </BrowserRouter>
