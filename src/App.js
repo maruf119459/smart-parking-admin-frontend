b92395e7a4eb538f162d95bb8a264b9bce94b80f
Abrarul-Haque1303 <abrararko8691@gmail.com>
2026-02-06 19:58:28 +0600

    Add Registration Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
+import PublicRoute from "./routes/PublicRoute"
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
 
         <Routes>
+          <Route path="/register" element={<PublicRoute> <Register /> </PublicRoute>} />
          
         </Routes>
       </BrowserRouter>
