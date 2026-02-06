6cb2f7f15ee614a52afbb0a69242d5532818f8bf
Abrarul-Haque1303 <abrararko8691@gmail.com>
2026-02-06 20:47:47 +0600

    Add Login & Forgot Password Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
-import PublicRoute from "./routes/PublicRoute"
+import PublicRoute from "./routes/PublicRoute";
+import PrivateRroute from "./routes/PrivateRoute";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+        <Navbar></Navbar>
 
         <Routes>
           <Route path="/register" element={<PublicRoute> <Register /> </PublicRoute>} />
+          <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+          <Route path="/forgot-password" element={<PublicRoute> <ForgotPassword /></PublicRoute>} />
          
         </Routes>
       </BrowserRouter>
