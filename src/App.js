ae429d43d2d30269c44f3cc15bebfe559784cb9b
maruf119459 <mdmohiuddinmaruf7@gmail.com>
2026-02-06 22:12:23 +0600

    Add Admin Management Feature / Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
 import PublicRoute from "./routes/PublicRoute";
-import PrivateRroute from "./routes/PrivateRoute";
+import PrivateRoute from "./routes/PrivateRoute";
 
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import AdminManagement from "./pages/AdminManagement";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
           <Route path="/register" element={<PublicRoute> <Register /> </PublicRoute>} />
           <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
           <Route path="/forgot-password" element={<PublicRoute> <ForgotPassword /></PublicRoute>} />
+         
+          <Route path="/AdminManagement" element={<PrivateRoute> <AdminManagement /> </PrivateRoute>} />
          
         </Routes>
       </BrowserRouter>
