bc0ce723db119887034d6b739c8efa68e275c812
maruf119459 <mdmohiuddinmaruf7@gmail.com>
2026-02-14 16:24:14 +0600

    Fix app js error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
 import PublicRoute from "./routes/PublicRoute";
 import PrivateRroute from "./routes/PrivateRoute";
-
+import Navbar from "./components/Navbar"
+import ForgotPassword from "./pages/ForgotPassword"
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 
