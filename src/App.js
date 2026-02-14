b6c0b62cc06bc415d199cb098859d4f218470023
maruf119459 <mdmohiuddinmaruf7@gmail.com>
2026-02-14 12:11:36 +0600

    Navbar Error Fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-
+<Navbar></Navbar>
         <Routes>
          
         </Routes>
