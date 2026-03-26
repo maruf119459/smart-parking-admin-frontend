470e985921a16a26cd0d55beb36f8a33d3ea1d87
Abrarul-Haque1303 <abrararko8691@gmail.com>
2026-03-26 04:18:03 +0000

    Update Socket by Live API

--- src/socket.js
+++ src/socket.js
@@ -1,2 +1,10 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:5000");
+const socket = io("https://smart-parking-backend-u47b.onrender.com", {
+  transports: ["websocket", "polling"],
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 2000,
+  timeout: 10000,
+  withCredentials: true
+});
+export default socket;
